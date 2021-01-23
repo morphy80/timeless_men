d3506d77056975e47a93445d7a3b0c61b3dae118
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2021-01-23 00:06:35 +0000

    added full shopping cart functionality and styled product detail page

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_cart(request):
@@ -33,3 +33,52 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    warranty = None
+    if 'product_warranty' in request.POST:
+        warranty = request.POST['product_warranty']
+    cart = request.session.get('cart', {})
+
+    if warranty:
+        if quantity > 0:
+            cart[item_id]['items_by_warranty'][warranty] = quantity
+        else:
+            del cart[item_id]['items_by_warranty'][warranty]
+            if not cart[item_id]['items_by_warranty']:
+                cart.pop(item_id)
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+        else:
+            cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+
+    try:
+        warranty = None
+        if 'product_warranty' in request.POST:
+            warranty = request.POST['product_warranty']
+        cart = request.session.get('cart', {})
+
+        if warranty:
+            del cart[item_id]['items_by_warranty'][warranty]
+            if not cart[item_id]['items_by_warranty']:
+                cart.pop(item_id)
+        else:
+            cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
